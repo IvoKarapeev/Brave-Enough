9039f08fa434a7caa6d587cf67ee69789d46c095
DarkHouse123 <ivokarapeev@gmail.com>
2022-06-21 19:05:52 +0300

    installing cookie-parser

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const { initizeDatabase } = require('../config/database');
 const { PORT } = require('../config/env');
 const router = require('./routes');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
 require('../config/handlebars')(app);
 
+app.use(cookieParser());
 app.use(router);
 
 initizeDatabase()
