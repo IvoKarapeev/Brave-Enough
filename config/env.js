deaf15a2f22d33e269d3d50fd017129c15eb656b
DarkHouse123 <ivokarapeev@gmail.com>
2022-06-27 11:50:36 +0300

    making userMiddlewares

--- config/env.js
+++ config/env.js
@@ -1,1 +1,3 @@
 exports.PORT = '3000';
+exports.COOKIE_SESSION_USER = 'user';
+exports.SECRET = 'AskmaKSMKAdfmsMASXL23A39Cssd93cm9dsaklm923'
---
+++ src/middlewares/userMiddleware.js
@@ -0,0 +1,45 @@
+const { COOKIE_SESSION_USER } = require('../../config/env');
+const { SECRET } = require('../../config/env');
+const jwt = require('jsonwebtoken');
+
+exports.auth = async ( req,res,next ) => {
+
+    const token = req.cookies[COOKIE_SESSION_USER];
+
+    if (token) {
+        jwt.verify( token, SECRET, (err,decodedToken) => {
+
+            if (err) {
+                res.clearCookie(COOKIE_SESSION_USER);
+                console.log(err);
+                // return next(err);
+                return res.redirect('login')
+            };
+
+
+            req.user = decodedToken;
+            res.locals.user = decodedToken;
+
+            next();
+        });
+    }else{
+
+        next();
+    }
+};
+
+exports.isAuth = ( req,res,next ) => {
+    if (!req.user) {
+        return res.redirect('/user/login');
+    }
+
+    next();
+};
+
+exports.isGuest = ( req,res,next ) => {
+    if (req.user) {
+        return res.redirect('/');
+    }
+
+    next();
+};
